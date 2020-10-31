6d725f77537e7e655102175d2ca5ea5eda75181a
SperaX <2388287586@qq.com>
2020-10-31 23:31:51 +0800

    Hystrix服务降级，生产模块和消费模块分别降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.learn.springcloud.controller;
 
 import com.learn.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,9 +25,16 @@ public class OrderHystrixController {
         String result=paymentHystrixService.payment_OK(id);
         return result;
     }
+    @HystrixCommand(fallbackMethod = "payment_TimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")  //3秒钟以内就是正常的业务逻辑
+    })
     @GetMapping(value = "/consumer/payment/Hystrix/timeout/{id}")
     public String payment_TimeOut(@PathVariable("id")Integer id){
+        int age=10/0;
         String result=paymentHystrixService.payment_TimeOut(id);
         return result;
+    }
+    public String payment_TimeOutFallbackMethod(Integer id){
+        return "我是消费者80，对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
     }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/learn/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/learn/springcloud/service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.learn.springcloud.service;
 
 import cn.hutool.core.thread.ThreadUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -8,15 +10,20 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class PaymentService {
     public String paymentInfo_OK(Integer id){
-        return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK，id："+id;
+        return "线程池："+Thread.currentThread().getName()+" paymentInfo_OK，id："+id+"O(∩_∩)O哈哈~";
     }
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")  //3秒钟以内就是正常的业务逻辑
+    })
     public String paymentInfo_TimeOut(Integer id){
-        int timeNumber=3;
         try {
-            TimeUnit.SECONDS.sleep(timeNumber);
+            TimeUnit.MILLISECONDS.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池："+Thread.currentThread().getName()+"paymentInfo_TimeOut，id："+id+"耗时(秒):"+timeNumber;
+        return "线程池："+Thread.currentThread().getName()+"paymentInfo_TimeOut，id："+id+"   O(∩_∩)O哈哈~";
+    }
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"8001系统繁忙，请稍后再试，id："+id+"    o(╥﹏╥)o";
     }
 }
